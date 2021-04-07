29d097583eecc9985c3eae558416a63a2df8d655
Umme Athiya <43459908+UMMEATHIYA@users.noreply.github.com>
2021-04-07 17:35:27 +0530

    commit

---
+++ JAVA FSD/April 6/activemq-receiver-service/src/test/java/com/example/activemqreceiverservice/ActivemqReceiverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.activemqreceiverservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivemqReceiverServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
